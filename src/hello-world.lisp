a965f0f4e1e6e31864b3c8d0a7c88d5d3bf1ae0a
Jose Santos <jsmpereira@gmail.com>
2012-03-13 15:08:43 +0000

    Show hunchentoot version.

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -2,7 +2,7 @@
 
 (hunchentoot:define-easy-handler (hello-sbcl :uri "/hello-sbcl") (name)
   (setf (hunchentoot:content-type*) "text/plain")
-  (format nil "Hey~@[ ~A~]! ~A ~A" name (lisp-implementation-type) (lisp-implementation-version)))
+  (format nil "Hey~@[ ~A~]! ~A ~A, Hunchentoot ~A" name (lisp-implementation-type) (lisp-implementation-version) hunchentoot::*hunchentoot-version*))
 
 ;(publish :path "/"
 ;	 :function #'(lambda (req ent)
