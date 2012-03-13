900aee9b02bc5aca95b131aca9f0881037b4d2b6
Jose Santos <jsmpereira@gmail.com>
2012-03-13 18:52:20 +0000

    Redefine handler for app root.

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -1,6 +1,6 @@
 (in-package :hunchentoot)
 
-(hunchentoot:define-easy-handler (hello-sbcl :uri "/hello-sbcl") (name)
+(hunchentoot:define-easy-handler (hello-sbcl :uri "/") (name)
   (setf (hunchentoot:content-type*) "text/plain")
   (format nil "Hey~@[ ~A~]! ~A ~A, Hunchentoot ~A" name (lisp-implementation-type) (lisp-implementation-version) hunchentoot::*hunchentoot-version*))		   
 
