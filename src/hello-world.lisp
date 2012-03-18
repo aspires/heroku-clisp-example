8df260b47d119ec1c3b5ee7ed4ac02a671b07f18
Jose Santos <jsmpereira@gmail.com>
2012-03-18 14:41:39 +0000

    Argh, typo.

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -1,6 +1,6 @@
 (in-package :example)
 
-(defvar *databse-url* "postgres://sjxorekfoz:AGMva3s8HwDwtaCqXE7m@ec2-107-20-191-127.compute-1.amazonaws.com/sjxorekfoz")
+(defvar *database-url* (sb-posix:getenv "DATABASE_URL"))
 
 (defun db-params ()
   (let* ((url (second (cl-ppcre:split "//" *database-url*)))
